0a760a08b5e88d7bf658729b44adb949a2d569ba
Oleg Filimontcev <oleg.filimontcev@gmail.com>
2018-07-13 03:16:34 +0300

    deploy fix #1

--- server.js
+++ server.js
@@ -30,12 +30,15 @@ app.use(bodyParser.json());
 app.use('/api/users', users);
 app.use('/api/companies', companies);
 
-// Set static folder
-app.use(express.static('client/build'));
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Listen to port
 const port = process.env.PORT || 5000;
